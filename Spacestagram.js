7e82abca718e1c7e2d87b4eda997c2ed6a6663ae
jcdedrick <79244032+jcdedrick@users.noreply.github.com>
2022-02-04 13:18:28 -0600

    Add files via upload

---
+++ Spacestagram.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import  {useState, useEffect } from 'react';
+import NavBar from "./NavBar";
+
+const apiKey = (process.env.REACT_APP_RAPID_NASA_KEY);
+
+export default function NasaImage(){
+const [photoData, setPhotoData] = useState(null);
+
+
+useEffect(() => {
+        fetchPhoto();
+
+        async function fetchPhoto() {
+            const res = await fetch(
+                'https://api.nasa.gov/planetary/apod?api_key=4wcP9NVEnyom0ACb1dmCR29kwn5AX1BadK0Kdybi'
+            );
+
+            const data = await res.json();
+            setPhotoData(data);
+            console.log(data);
+
+        }
+
+    },[]);
+
+if (!photoData) return <div />;
+
+
+return( 
+    <><NavBar /><div>
+        {photoData.media_type = "image" ? (
+            <img src={photoData.url} alt={photoData.title} />
+
+        ) : (
+            <iframe
+                title="space-video"
+                src={photoData.url}
+                frameBorder="0"
+                gesture="media"
+                allowFullScreen
+                className="photo" />
+
+
+        )}
+        <div>
+            <><h1>{photoData.title}</h1><p>{photoData.date}</p><p>{photoData.explanation}</p></>
+        </div>
+        
+
+
+    </div></>
+)
+}
